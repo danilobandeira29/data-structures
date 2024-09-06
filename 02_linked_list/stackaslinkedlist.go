c63d31cbb366ff4b7dfd85f536529a69585bde7b
Danilo Bandeira <danilobandeira29@gmail.com>
2024-09-06 16:53:06 -0300

    feat: added stack implemented as linked list

---
+++ 02_linked_list/stackaslinkedlist.go
@@ -0,0 +1,61 @@
+package linkedlist
+
+import (
+	"errors"
+)
+
+type StackAsLinkedList struct {
+	// do not need top, because start already solve the problem to run the linked list
+	top   *Node
+	start *Node
+	total int
+}
+
+var (
+	MAX_SIZE     = 10
+	ErrStackFull = errors.New("stackaslinkedlist: stack is full")
+	ErrEmptyPop  = errors.New("stackaslinkedlist: list must not be empty in order to pop")
+)
+
+func NewStackAsLinkedList() *StackAsLinkedList {
+	return &StackAsLinkedList{
+		top:   nil,
+		start: nil,
+		total: 0,
+	}
+}
+
+func (s *StackAsLinkedList) Push(n int) error {
+	if s.IsFull() {
+		return ErrStackFull
+	}
+	node := NewNode(n)
+	if s.IsEmpty() {
+		s.start = node
+	}
+	s.top = node
+	s.total++
+	return nil
+}
+
+func (s *StackAsLinkedList) Pop() (int, error) {
+	if s.IsEmpty() {
+		return -1, ErrEmptyPop
+	}
+	v := s.top.value
+	aux := s.start
+	for aux.Next() != nil {
+		aux = aux.Next()
+	}
+	s.top = aux
+	s.total--
+	return v, nil
+}
+
+func (s *StackAsLinkedList) IsFull() bool {
+	return s.total == MAX_SIZE
+}
+
+func (s *StackAsLinkedList) IsEmpty() bool {
+	return s.top == nil
+}
---
+++ 02_linked_list/stackaslinkedlist_test.go
@@ -0,0 +1,27 @@
+package linkedlist
+
+import "testing"
+
+func TestStackAsLinkedList(t *testing.T) {
+	s := NewStackAsLinkedList()
+	if !s.IsEmpty() {
+		t.Error("stackaslinkedlist: stack must be empty when instantiate")
+	}
+	elements := []int{10, 22, 33}
+	for _, e := range elements {
+		err := s.Push(e)
+		if err == ErrStackFull {
+			t.Error(err)
+		}
+	}
+	v, err := s.Pop()
+	if err == ErrEmptyPop {
+		t.Error(err)
+	}
+	if v != 33 {
+		t.Errorf("stackaslinkedlist: first element to remove expected to %d got instead %d\n", 33, v)
+	}
+	if s.IsEmpty() {
+		t.Error("stackaslinkedlist: must have element, because not all elements has been poped")
+	}
+}
