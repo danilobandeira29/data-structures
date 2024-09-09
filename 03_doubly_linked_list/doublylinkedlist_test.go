5e687533f23690e6c0c7ddb1aa11badb9fb6e902
Danilo Bandeira <danilobandeira29@gmail.com>
2024-09-09 18:43:05 -0300

    test: doublylinkedlist

--- 03_doubly_linked_list/doublylinkedlist_test.go
+++ 03_doubly_linked_list/doublylinkedlist_test.go
@@ -24,11 +24,23 @@ func TestDoublyLinkedList(t *testing.T) {
 	if element != 44 {
 		t.Errorf("doublylinkedlist: expect %d but got %d\n", 44, element)
 	}
+	if !slices.Equal(dll.InOrder(), []int{55, 33, 22}) {
+		t.Error("doublylinkedlist: not in order after remove from start")
+	}
+	if !slices.Equal(dll.Reverse(), []int{22, 33, 55}) {
+		t.Error("doublylinkedlist: not in reverse order after remove from start")
+	}
 	element, err = dll.RemoveFromEnd()
 	if err != nil {
 		t.Error("doublylinkedlist: not possible to remove from end")
 	}
 	if element != 22 {
 		t.Errorf("doublylinkedlist: expect %d but got %d\n", 22, element)
+	}
+	if !slices.Equal(dll.InOrder(), []int{55, 33}) {
+		t.Error("doublylinkedlist: not in order after remove from end")
+	}
+	if !slices.Equal(dll.Reverse(), []int{33, 55}) {
+		t.Error("doublylinkedlist: not in reverse order after remove from end")
 	}
 }
