650ae62851aed1a3aa508b5a746d954683a4d054
Danilo Bandeira <danilobandeira29@gmail.com>
2024-09-10 16:08:25 -0300

    refactor: substitute algorithm

--- 05_bubble_sort/bubblesort.go
+++ 05_bubble_sort/bubblesort.go
@@ -1,15 +1,12 @@
 package bubblesort
 
-func Sort(values []int) []int {
+func Sort(values []int) {
 	for i := 0; i < len(values)-1; i++ {
 		for j := 0; j < len(values)-i-1; j++ {
 			if values[j] <= values[j+1] {
 				continue
 			}
-			temp := values[j]
-			values[j] = values[j+1]
-			values[j+1] = temp
+			values[j], values[j+1] = values[j+1], values[j]
 		}
 	}
-	return values
 }
