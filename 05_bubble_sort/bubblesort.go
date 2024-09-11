807ab3e466d5ba8ac9493743ab10e501b545346f
Danilo Bandeira <danilobandeira29@gmail.com>
2024-09-11 14:08:27 -0300

    feat: added SortFunc to bubblesort and selectionsort

--- 05_bubble_sort/bubblesort.go
+++ 05_bubble_sort/bubblesort.go
@@ -9,3 +9,14 @@ func Sort(values []int) {
 		}
 	}
 }
+
+func SortFunc[T any](v []T, f func(a, b T) int) {
+	for i := 0; i < len(v)-1; i++ {
+		for j := 0; j < len(v)-i-1; j++ {
+			result := f(v[j], v[j+1])
+			if result > 0 {
+				v[j], v[j+1] = v[j+1], v[j]
+			}
+		}
+	}
+}
--- 06_selection_sort/selectionsort.go
+++ 06_selection_sort/selectionsort.go
@@ -13,3 +13,19 @@ func Sort(v []int) {
 		}
 	}
 }
+
+func SortFunc[T any](v []T, f func(a, b T) int) {
+	for i := 0; i < len(v)-1; i++ {
+		minIdx := i
+		for j := i + 1; j < len(v); j++ {
+			result := f(v[minIdx], v[j])
+			if result > 0 {
+				minIdx = j
+			}
+		}
+		result := f(v[i], v[minIdx])
+		if result > 0 {
+			v[minIdx], v[i] = v[i], v[minIdx]
+		}
+	}
+}
